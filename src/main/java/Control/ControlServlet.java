1fe91a7b0085f4766264a473c5fa28b2f327f643
kristad <kristad@hotmail.it>
2014-10-31 20:15:53 +0100

    user

--- src/main/java/Control/ControlServlet.java
+++ src/main/java/Control/ControlServlet.java
@@ -18,6 +18,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import metier.User;
+import domain.DAOUser;
 
 
 /**
@@ -59,29 +63,123 @@ public class ControlServlet extends HttpServlet {
             Class.forName("com.mysql.jdbc.Driver");
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://"+host+":"+port+"/dar", "adminc1A7TAm", "6gG4scG6dM1J");
-            Statement stmt = conn.createStatement();
-            String query = "select * from contrat ;";
-            ResultSet resultset = stmt.executeQuery(query);
+            HttpSession session; // il faut ajouter l'utilisation de cookies et url et autres ?
+    		//int  visitCount=0;
+    		 
+    		String email=request.getParameter("email");
+    		String password=request.getParameter("password");
+    		
+    		String password2=request.getParameter("password2");
+    		String name=request.getParameter("name");
+    		String firstname=request.getParameter("firstname");
+    		String datenaissance=request.getParameter("datenaissance");
+    		String sexe=request.getParameter("sexe");
+    		
+    		String insc=request.getParameter("inscription");
+    		String con=request.getParameter("connect");
+    		
+    		if((insc == null && con != null) || (insc != null && con == null)){ // un seul bouton clické
 
-            
-            response.setContentType("text/html"); 
-            out.println("jdbc:mysql://"+host+":"+port+"/dar");
-            int i = 0;
-            while (resultset.next()) {
+    			 DAOUser du = new DAOUser();
+    			 User u;
+    		if(insc == null && con != null){ // connection
+    			if(!(email.equals("")) && !(password.equals(""))){
+    				
+    				// verifier patern mail
+    				// et password, minimum 4 caracteres -----------
+    				try{
+    				 u=du.getUserByEmail(email); // connexion bd
+    				 if(u != null){
+    					 if(password.equals(u.getPass())){ // verifier password et hasher !
+    						 
+    						// ********** a ajouter, inactiver l'ancienne et créer une nouvelle session, ?
+    						 // RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html"); // revoyer la requete
+    						 
+    						 // il faut toujours donner à l'utilisateur la possibilité de se déconnecter ou savoir qu'il est connecté
+    						try{
+    						 session = request.getSession(false); // détruire les anciennes sessions
+    						 session.invalidate();
+    						}
+    						catch(Exception e){
+    						}
+    						 session = request.getSession(true);
+    						 response.getWriter().write(u.toStrig()); 
+    				         session.setAttribute("userID", u.getId());
+    						 
+    				         /*
+    						 if (session.isNew()){
+    					         session.setAttribute("userID", u.getId());
+    					      } else {
+    					      }
+    							// visitCount = (Integer)session.getAttribute("visitCountKey")+1;
+    					        //  userID = (String)session.getAttribute(userID);
+    					     	// session.setAttribute("visitCountKey",  visitCount);	 
+    					      */
+    					      
+    						 
+    					 }else{
+    						 response.getWriter().write("mot de pass éronné");
+    					 }
+    				 }else{
+    					 response.getWriter().write("utilisateur introuvable");
+    				 }
+    				}catch(Exception e){
+    					response.getWriter().write("utilisateur introuvable"); // wait et redirect
+    				}
 
-                String id = resultset.getString(1);
-                out.println(id);
-                String id1 = resultset.getString(2);
-                out.println(id1);
-                String id2 = resultset.getString(3);
-                out.println(id2);
-                i = i + 1;
-
-            }
-            out.println("Vous avez " + i + " Enregistrement");
-
-            stmt.close();
-            conn.close();
+    			}else{
+    				response.sendRedirect("index.html");
+    			}
+    		}else{ // inscription
+    			
+    			if(sexe == null || !(password.equals(password2)) || email.equals("") || password.equals("") || password2.equals("") || name.equals("") || firstname.equals("") || datenaissance.equals("")){
+    				response.sendRedirect("index.html");
+    			}else{
+    				//a faire, verifier le reste des paterns
+    				// exécuter--------
+    				
+    				u=du.getUserByEmail(email);
+    				int j=0;
+    				int s=10;
+    				if(sexe.equals("homme")){ // completer ------------------
+    					s=1;
+    				}else{
+    					s=0;
+    				}
+    				try{
+    					j=Integer.parseInt(datenaissance);
+    				}catch (Exception e){
+    					response.sendRedirect("index.html");
+    				}
+    				if(u == null && j != 0 && j<100000000 && j>= 10000000){ // inscrire
+    					u=du.addUser(firstname, name, email, password, s, j, null);
+    					
+    				
+    					
+    					 session = request.getSession(true);
+    	
+    					 if (session.isNew()){
+    				         session.setAttribute("userID", u.getId());
+    				      } else { // seul moyen utilisé pour se déconncecter automatiquement sinon, session reste ouverte?
+    				     // visitCount = (Integer)session.getAttribute("visitCountKey")+1;
+    				       //  userID = (String)session.getAttribute(userID);
+    				    	  // je ne fais pas inactivate ici
+    					      session.setAttribute("userID", u.getId()); // ce n'est pas important de donner à un navigateur la possibilité de créer plussieures comptes, mais juste pour le teste
+    				      }
+    				      //session.setAttribute("visitCountKey",  visitCount);
+    					 try{
+    						wait(5000);
+    					 }catch(Exception e){ 
+    					 }
+    						response.sendRedirect("Choix_contrat");
+    					
+    					
+    				}else{ // utilisateur éxistant
+    					response.getWriter().write("email dejas utilisé");
+    				}
+    					
+    			}	
+    		}}	
         } catch (Exception e) {
             e.printStackTrace();
         }
